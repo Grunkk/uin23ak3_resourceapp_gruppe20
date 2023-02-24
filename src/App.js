8a754bb25d72d1f906d1b8ec73e38967f5948319
MathiasJohansen <mathiaslj2011@hotmail.no>
2023-02-24 13:17:05 +0100

    fikla litt og installer route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Layout from './components/Layout';
+import Main from './components/Main'
 import {Route, Routes} from 'react-router-dom'
+import ResourcePage from './components/ResourcePage';
 
 function App() {
   return (
   
     <Routes>
-      <Route>
-        
+      <Route element={<Layout/>}>
+        <Route index element={<Main />} />
+        <Route path=':slug' element={<ResourcePage recipes={recipes}  />}/>
       </Route>
     </Routes>  
-    
-    <Layout/>
+
       );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,9 +3,7 @@ import resources from '../ressurser/ressurser.js';
 export default function Main({resources}){
     return(
         <>
-        {resources.map((resource) => (
-            
-        ))}
+       
         </>
     )
 }
