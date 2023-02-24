e0fa4c0f3337792e60fe88c0522f16231df8942a
MathiasJohansen <mathiaslj2011@hotmail.no>
2023-02-24 13:05:49 +0100

    lagd resourcepage og prøver meg på routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Layout from './components/Layout';
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
+  
+    <Routes>
+      <Route>
+        
+      </Route>
+    </Routes>  
+    
     <Layout/>
-  );
+      );
 }
 
 export default App;
