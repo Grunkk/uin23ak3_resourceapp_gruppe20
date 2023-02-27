4938684eb74972a347ee0ea72d5f2a8ff2740019
Joachim Bjerkland <Bjerkland2011@hotmail.no>
2023-02-27 11:05:36 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import './App.js';
+import logo from './logo.svg';
+import './App.css';
+import './css/main.css'
 import Layout from './components/Layout';
 import Main from './components/Main'
+
 import {Route, Routes} from 'react-router-dom'
+import ResourcePage from './components/ResourcePage';
+import { useState } from 'react';
 
 function App() {
+  const [resources] = useState([])
   return (
   
     <Routes>
       <Route element={<Layout/>}>
-        <Route index element={<Main />} />
-        <Route />
+        <Route index element={<Main resources={resources} />} />
+        <Route path=':slug' element={<ResourcePage resources={resources}/>}/>
       </Route>
     </Routes>  
 
